688a4d93830b0dbe55a197752e893d8e8615a7d5
semeon <semeon.peradze@gmail.com>
2015-03-22 23:41:47 -0700

    updating template

--- source/RobotAlpha.ino
+++ source/RobotAlpha.ino
@@ -1,36 +1,23 @@
 #include <Arduino.h>
 #include "LED.h"
-#include "BTN.h"
-#include "TrafficLight.h"
+#include "wheel.h"
+// #include "BTN.h"
+// #include "TrafficLight.h"
 
-TrafficLight tl = TrafficLight();
+const int ledPin =  13;      // the number of the LED pin
 
-// the setup routine runs once when you press reset ================================
-void setup() {   
-  tl.init(3);
+
+void setup() {
+
+
 }
 
-// the loop routine runs over and over again forever =================================
-void loop() {
-  unsigned long currentTimestamp = millis();
+void loop()
+{
 
-  switch (tl.getMode()) {
-    case 1:
-      tl.lightChangeRoutine();
-      tl.switchToBlinkingGreen();
-      break;
 
-    case 0:
-      // Blink green
-      if (tl.greenLed.isTimeToChange(currentTimestamp)){
-        tl.greenLed.toggle();
-      }
 
-      // Check for button click
-      if (tl.button.clicked()) {     
-        // turn LED on:    
-        tl.startLightChangeRoutine();
-      }
-      break;
-  }
 }
+
+
+
